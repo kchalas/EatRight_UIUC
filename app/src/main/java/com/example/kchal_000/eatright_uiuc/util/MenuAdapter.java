cc3c87f1f351b94856d38780311d231e0ef53376
kchalas <kchalas@knox.edu>
2015-03-11 16:15:27 -0500

    Attempting to create a listView to add to visualization

---
+++ app/src/main/java/com/example/kchal_000/eatright_uiuc/util/MenuAdapter.java
@@ -0,0 +1,68 @@
+package com.example.kchal_000.eatright_uiuc.util;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by kchal_000 on 3/11/2015.
+ */
+public class MenuAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> listGroup;
+    private HashMap<String, List<String>> listChild;
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
