8345ddfad89bedf146cec61d16a27f42308e5ab3
Clarence <elliot20@illinois.edu>
2015-04-13 16:40:39 -0500

    Initial UI Tests - elliot20

--- app/src/androidTest/java/com/example/kchal_000/eatright_uiuc/UITest.java
+++ app/src/androidTest/java/com/example/kchal_000/eatright_uiuc/UITest.java
@@ -16,6 +16,7 @@ public class UITest extends UiAutomatorTestCase{
     public void testAppOpen() throws UiObjectNotFoundException {
         // First we testing the press of the HOME button.
         getUiDevice().pressHome();
+        getUiDevice().pressHome();
 
         //the "applications" has the content-desc “Apps"
         UiObject Applications = new UiObject(new UiSelector().description("Apps"));
@@ -23,7 +24,7 @@ public class UITest extends UiAutomatorTestCase{
         // testing the click to bring up the All Apps screen.
         Applications.clickAndWaitForNewWindow();
 
-        UiObject apps = new UiObject(new UiSelector().text("Apps"));
+        UiObject apps = new UiObject(new UiSelector().description("Apps"));
 
         apps.click(); //In the Apps Tab
 
@@ -39,6 +40,7 @@ public class UITest extends UiAutomatorTestCase{
     }
 
     public void testAppHomeScreen() throws UiObjectNotFoundException{
+
         testAppOpen();
         UiObject BackToRestaurantScreen = new UiObject(new UiSelector().resourceId("com.example.kchal_000.eatright_uiuc:id/buttonBack"));
         BackToRestaurantScreen.click();
@@ -46,7 +48,6 @@ public class UITest extends UiAutomatorTestCase{
     }
 
     public void testRestaurantSelection1() throws UiObjectNotFoundException{
-        testAppOpen();
         testAppHomeScreen();
 
         UiObject Maize = new UiObject(new UiSelector().className("android.widget.Button").index(1));
@@ -54,7 +55,73 @@ public class UITest extends UiAutomatorTestCase{
 
     }
 
+    public void testDisplayLowCalItem() throws UiObjectNotFoundException{
+        testRestaurantSelection1();
 
+        UiObject expandLowCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                                                               .textContains("Low Cal - 249 or less"));
+        expandLowCal.click();
+
+        UiObject expandPepsi = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("pepsi"));
+        expandPepsi.click();
+
+
+
+
+    }
+
+    public void testExpandMenuList() throws UiObjectNotFoundException{
+        testRestaurantSelection1();
+
+        UiObject expandLowCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("Low Cal - 249 or less"));
+        expandLowCal.click();
+
+        UiObject expandMedCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("Medium Cal - 250 to 499"));
+        expandMedCal.click();
+
+        UiObject expandVeryHighCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("Very High Cal - 750 or more "));
+        expandVeryHighCal.click();
+
+    }
+
+    public void testRetractMenuList() throws UiObjectNotFoundException{
+        testExpandMenuList();
+
+
+        UiObject retractLowCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("Low Cal - 249 or less"));
+        retractLowCal.click();
+
+        UiObject retractMedCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("Medium Cal - 250 to 499"));
+        retractMedCal.click();
+
+        UiObject retractVeryHighCal = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("Very High Cal - 750 or more "));
+        retractVeryHighCal.click();
+    }
+
+    public void testCheckBoxSelection() throws UiObjectNotFoundException{
+            testDisplayLowCalItem();
+
+        UiObject expandPepsi = new UiObject( new UiSelector().className("android.widget.TextView")
+                .textContains("pepsi"));
+
+       UiObject pepsiCheck =  expandPepsi.getFromParent(new UiSelector().className("android.widget.CheckBox"));
+       pepsiCheck.click();
+    }
+
+    public void testVisualization() throws UiObjectNotFoundException{
+
+    }
+
+    public void resetDeviceUI() throws UiObjectNotFoundException{
+        getUiDevice().pressHome();
+    }
 
     /*
     public void testRestaurantSelection2() throws UiObjectNotFoundException{
