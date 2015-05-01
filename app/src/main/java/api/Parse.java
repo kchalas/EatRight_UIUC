b74f3dae8d95e5672ff1ab24354add38da8f6518
Alhaji <mansara2@illinois.edu>
2015-04-30 19:52:38 -0500

    Parser work

--- app/src/main/java/api/Parse.java
+++ app/src/main/java/api/Parse.java
@@ -58,7 +58,7 @@ public class Parse {
     public static ArrayList<MenuItem> parseMenu(String jsonData){
         try {
             ArrayList<MenuItem>  menuList = new ArrayList<MenuItem> ();
-            MenuItem entry= new MenuItem();
+
             JSONObject obj = new JSONObject(jsonData);
             JSONArray venues = obj.getJSONArray("venues");
             System.out.println(jsonData);
@@ -67,14 +67,18 @@ public class Parse {
                     continue;
                 }
                 JSONArray menus = venues.getJSONObject(l).getJSONArray("menu_items");
+                Log.i("menu",menus.toString());
                 for (int z = 0; z < menus.length(); z++) {
+                    MenuItem entry= new MenuItem();
                     String name = menus.getJSONObject(z).get("name").toString();
+                    Log.i("menu",name);
                     entry.setName(name);
                     menuList.add(entry);
                 }
             }
             return menuList;
-        }catch(JSONException e){
+        }
+        catch(JSONException e){
             Log.e("error", e.toString());
         }
 
